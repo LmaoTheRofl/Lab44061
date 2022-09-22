7926fa10dc6c1b6fe4175265945be8994e653897
LmaoTheRofl <Zharkov.nik04@mail.ru>
2022-09-22 15:58:50 +0300

    cbrtfix2

--- src/Lab1.java
+++ src/Lab1.java
@@ -15,13 +15,13 @@ public class Lab1 {
     for (int i = 0; i<16; i++){
         for (int j = 0; j<18; j++){
             if (c[i] == 7){
-                d[i][j] = Math.pow(Math.E, Math.atan((2/3)*((x[j]-1)/18)));
+                d[i][j] = Math.pow(Math.E, Math.atan((2.0/3)*((x[j]-1)/18)));
             }
             else if (c[i]==3 | c[i]==4 | c[i]==6 | c[i]==9 | c[i]==10 | c[i]==12 | c[i]==14 | c[i]==16) {
-                d[i][j] = (Math.pow(Math.atan((x[j]-1)/18), (1/3)))/2;
+                d[i][j] = (Math.cbrt(Math.atan((x[j]-1)/18)))/2;
             }
             else {
-                d[i][j] = Math.cos(Math.cbrt(Math.pow(((1/4)*Math.sin(x[j])), Math.cos(x[j]))));
+                d[i][j] = Math.cos(Math.cbrt(Math.pow(((1.0/4)*Math.sin(x[j])), Math.cos(x[j]))));
             }
         }
     }
